b01d8641e787b55f4a0fc78f82f554a670f07113
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-06-25 21:30:05 +0300

    new ip lesson

--- NullTerminatedLinesLesson/main.cpp
+++ NullTerminatedLinesLesson/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<Windows.h>
 using namespace std;
-
+#define DDD
 int StringLength(const char str[]);
 char* ToUpper(char str[]);
 char* ToLower(char str[]);
@@ -37,8 +37,9 @@ void main()
 	if (Is_int_number(str)) cout << "to int number: " << To_int_number(str) + 222 << endl;
 	if (Is_bin_number(str)) cout << "bin_to_int: " << Bin_to_dec(str) << endl;
 	if (Is_hex_number(str)) cout << "Hex_to_int: " << hex_to_dec(str) << endl;
-	cout << "IPv4: " << (isIPaddress(str) ? str : "not") << endl;
-	cout << " atoi " << atoi(str) << endl;
+	//cout << "IPv4: " << (isIPaddress(str) ? str : "not") << endl;
+	//cout << " atoi " << atoi(str) << endl;
+	cout << (isIPaddress(str) ? "Yes" : "no...") << endl;
 	SetConsoleCP(866);
 }
 int StringLength(const char str[])
@@ -200,68 +201,92 @@ int hex_to_dec(char str[])
 	return decimal;
 
 }
-bool isIPaddress(const char str[])//проверка: only цифры и точки, only три точки, числа между точками от 0 до 255
-{
-	int n = StringLength(str);
-	int cont = 0;
-	//bool str_int = 1;
-	int str_id = 0;
-	if (n <= 15)
-	{
-		for (int ii = 0; str[ii] != '.'; ii++)
-		{
-			str_id *= 10;
-			str_id += (int(str[ii] - 48));
-			if (str_id > 255)return 0;
-		}
-	}
-	else return 0;
-	for (int i = 0; str[i]; i++)
-	{
-		if ((str[i] >= '0' && str[i] <= '9') || str[i] == '.')
-		{
-
-			if (str[i] == '.')
-			{
-				if (str[i + 1] == '.')return 0;
-				if (str[i + 1] == 0)
-				{
-					cout << "end" << endl;
-					return 0;
-				}
-				str_id = 0;
-				for (int ii = i; str[ii + 1]; ii++)
-				{
-					cout << "------ ii+" << str[ii + 1] << endl;
-					if (str[ii + 1] == '.')continue;
-					cout << "ii: " << ii << " STR+ " << str[ii + 1] << " str_id: " << str_id << endl;
-					str_id *= 10;
-					str_id += (int(str[ii + 1] - 48));
-
-					cout << "str_id: " << str_id << endl;
-					if (str_id > 255)
-					{
-						cout << "255--" << endl;
-						return 0;
-					}
-				}
-				cont++;
-			}
-		}
-		else return 0;
-
-	}
-	if (cont > 0 && cont <= 3)
-	{
-		cout << "cont= " << cont << endl;
-		return 1;
-	}
-	else return 0;
-	return 1;
-}
+//bool isIPaddress(const char str[])//проверка: only цифры и точки, only три точки, числа между точками от 0 до 255
+//{
+//	int n = StringLength(str);
+//	int cont = 0;
+//	//bool str_int = 1;
+//	int str_id = 0;
+//	if (n <= 15)
+//	{
+//		for (int ii = 0; str[ii] != '.'; ii++)
+//		{
+//			str_id *= 10;
+//			str_id += (int(str[ii] - 48));
+//			if (str_id > 255)return 0;
+//		}
+//	}
+//	else return 0;
+//	for (int i = 0; str[i]; i++)
+//	{
+//		if ((str[i] >= '0' && str[i] <= '9') || str[i] == '.')
+//		{
+//
+//			if (str[i] == '.')
+//			{
+//				if (str[i + 1] == '.')return 0;
+//				if (str[i + 1] == 0)
+//				{
+//					cout << "end" << endl;
+//					return 0;
+//				}
+//				str_id = 0;
+//				for (int ii = i; str[ii + 1]; ii++)
+//				{
+//					cout << "------ ii+" << str[ii + 1] << endl;
+//					if (str[ii + 1] == '.')continue;
+//					cout << "ii: " << ii << " STR+ " << str[ii + 1] << " str_id: " << str_id << endl;
+//					str_id *= 10;
+//					str_id += (int(str[ii + 1] - 48));
+//
+//					cout << "str_id: " << str_id << endl;
+//					if (str_id > 255)
+//					{
+//						cout << "255--" << endl;
+//						return 0;
+//					}
+//				}
+//				cont++;
+//			}
+//		}
+//		else return 0;
+//
+//	}
+//	if (cont > 0 && cont <= 3)
+//	{
+//		cout << "cont= " << cont << endl;
+//		return 1;
+//	}
+//	else return 0;
+//	return 1;
+//}
 bool isMACaddress(const char str[])
 {
 
 	return 0;
+}
+bool isIPaddress(const char str[])
+{
+	int n = strlen(str);
+	if (n < 7 || n>15)return false;
+	char bytes[4][4] = {};
+	for (int i = 0, j = 0,k=0; str[i]; i++)
+	{
+		if (str[i] == '.')
+		{
+			j++;
+			if (j > 3)return false;
+			k = 0;
+			continue;
+		}
+		//bytes[j][k++] = str[i];
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if (To_int_number(bytes[i]) > 255)return false;
+		cout << bytes[i] << "\t";
+	}
+	cout << endl;
+	return 1;
 }
 
