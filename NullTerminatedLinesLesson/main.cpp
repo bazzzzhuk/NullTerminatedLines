c7297a13a47f478896600021bbf2680e06018f42
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-06-25 20:50:57 +0300

    ++

--- NullTerminatedLinesLesson/main.cpp
+++ NullTerminatedLinesLesson/main.cpp
@@ -9,11 +9,11 @@ char* Shrink(char str[]);
 bool isPalindrom(const char str[]);
 //char* RemSymbol(char str[], const char symbol);
 bool Is_int_number(const char str[]);
-char* To_int_number(char str[]);
+int To_int_number(char str[]);
 bool Is_bin_number(const char str[]);
-char* Bin_to_dec(char str[]);
+int Bin_to_dec(char str[]);
 bool Is_hex_number(const char str[]);
-char* hex_to_dec(char str[]);
+int hex_to_dec(char str[]);
 //////////////////////////////////////
 bool isIPaddress(const char str[]);
 bool isMACaddress(const char str[]);
@@ -27,16 +27,18 @@ void main()
 	cout << "Enter str: ";
 	SetConsoleCP(1251);
 	cin.getline(str, SIZE);
-	cout << str << endl;
+	//cout << int(str[0]) << endl;
 	cout << StringLength(str) << endl;
 	cout << strlen(str) << endl;
 	ToUpper(str);
 	cout << ToUpper(str) << endl;
 	cout << Shrink(str) << endl;
 	cout << "Строка " << str << (isPalindrom(str) ? " " : " Не") << " является палиндромом" << endl;
-	if (Is_int_number(str)) cout << "to int number: " << To_int_number(str) << endl;
+	if (Is_int_number(str)) cout << "to int number: " << To_int_number(str) + 222 << endl;
+	if (Is_bin_number(str)) cout << "bin_to_int: " << Bin_to_dec(str) << endl;
+	if (Is_hex_number(str)) cout << "Hex_to_int: " << hex_to_dec(str) << endl;
 	cout << "IPv4: " << (isIPaddress(str) ? str : "not") << endl;
-
+	cout << " atoi " << atoi(str) << endl;
 	SetConsoleCP(866);
 }
 int StringLength(const char str[])
@@ -108,15 +110,16 @@ bool Is_int_number(const char str[])
 	for (int i = 0; str[i]; i++)if (str[i] >= '0' && str[i] <= '9'); else return 0;
 	return 1;
 }
-char* To_int_number(char str[])
+int To_int_number(char str[])// = atoi()
 {
+	if (!Is_int_number(str))return INT_MIN;
 	int n = 0;
 	for (int i = 0; str[i]; i++)
 	{
 		n *= 10;
-		n += ((int)str[i] - 48);
+		n += (str[i] - '0');
 	}
-	return str;
+	return n;
 
 }
 bool Is_bin_number(const char str[])
@@ -124,7 +127,8 @@ bool Is_bin_number(const char str[])
 	for (int i = 0; str[i]; i++)if (str[i] != '0' && str[i] != '1')return 0;
 	return 1;
 }
-char* Bin_to_dec(char str[])
+
+int Bin_to_dec(char str[])
 {
 	if (Is_bin_number(str))
 	{
@@ -134,8 +138,9 @@ char* Bin_to_dec(char str[])
 			int st = 1;
 			for (int ii = i - 1; ii >= 0; ii--)st *= 2;
 			n += (st * (int)(str[StringLength(str) - i - 1] - 48));
+			//n = n * 2 + str[i] - '0';//lesson 0 to str[i]
 		}
-		return str;
+		return n;
 	}
 }
 bool Is_hex_number(const char str[])
@@ -156,9 +161,9 @@ bool Is_hex_number(const char str[])
 	}
 	return 1;
 }
-char* hex_to_dec(char str[])
+int hex_to_dec(char str[])
 {
-	if (Is_hex_number(str))
+	/*if (Is_hex_number(str))
 	{
 		int n = 0;
 		char HEX[] = "0123456789ABCDEF";
@@ -179,21 +184,28 @@ char* hex_to_dec(char str[])
 			}
 			int st = 1;
 			for (int ii = i - 1; ii >= 0; ii--)st *= 16;
-			//cout << "st= " << st << endl;
 			n += (st * Hex);
-			//cout << "n= " << n << endl;
 		}
-		return str;
+		return n;
+	}*/
+	int decimal = 0;
+	for (int i = str[0] == '0' && str[1] == 'x' ? 2 : 0; str[i]; i++)
+	{
+		char hex_digit = str[i];
+		if (hex_digit >= '0' && hex_digit <= '9')hex_digit -= '0';
+		if (hex_digit >= 'A' && hex_digit <= 'F')(hex_digit -= 'A') += 10;
+		if (hex_digit >= 'a' && hex_digit <= 'f')(hex_digit -= 'a') += 10;
+		decimal = decimal * 16 + hex_digit;
 	}
+	return decimal;
+
 }
-bool isIPaddress(const char str[])//проверка: цифры и точки, три точки, числа между точками от 0 до 255
+bool isIPaddress(const char str[])//проверка: only цифры и точки, only три точки, числа между точками от 0 до 255
 {
 	int n = StringLength(str);
 	int cont = 0;
 	//bool str_int = 1;
 	int str_id = 0;
-	if (n <= 15)cout << "n: " << n << endl;
-	cout << int(str[n]) << endl;
 	if (n <= 15)
 	{
 		for (int ii = 0; str[ii] != '.'; ii++)
@@ -211,17 +223,20 @@ bool isIPaddress(const char str[])//проверка: цифры и точки, три точки, числа ме
 
 			if (str[i] == '.')
 			{
+				if (str[i + 1] == '.')return 0;
+				if (str[i + 1] == 0)
+				{
+					cout << "end" << endl;
+					return 0;
+				}
 				str_id = 0;
 				for (int ii = i; str[ii + 1]; ii++)
 				{
 					cout << "------ ii+" << str[ii + 1] << endl;
-					if (str[ii + 1] == '.' && str[ii] == '.')return 0;
-					if ((str[ii] == '.' && str[ii + 1]== n))return 0;
-
-					if (str[ii+1] == '.')continue;
-					cout <<"ii: "<<ii << " STR+ " << str[ii + 1] << " str_id: " << str_id << endl;
+					if (str[ii + 1] == '.')continue;
+					cout << "ii: " << ii << " STR+ " << str[ii + 1] << " str_id: " << str_id << endl;
 					str_id *= 10;
-					str_id += (int(str[ii+1] - 48));
+					str_id += (int(str[ii + 1] - 48));
 
 					cout << "str_id: " << str_id << endl;
 					if (str_id > 255)
@@ -234,10 +249,11 @@ bool isIPaddress(const char str[])//проверка: цифры и точки, три точки, числа ме
 			}
 		}
 		else return 0;
-		
+
 	}
 	if (cont > 0 && cont <= 3)
 	{
+		cout << "cont= " << cont << endl;
 		return 1;
 	}
 	else return 0;
