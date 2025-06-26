1eac0ce337460be79be910efd3a820fa323da460
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-06-27 02:16:14 +0300

    eklmn

--- NullTerminatedLinesLesson/main.cpp
+++ NullTerminatedLinesLesson/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<Windows.h>
 #include<string.h>
 using namespace std;
@@ -23,8 +23,8 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	const int SIZE = 256;
-	//char str[SIZE] = "Хорошо      живет     на     свете      Винни      Пух";
-	char str[] = "Аргентина манит негра";
+	//char str[SIZE] = "РҐРѕСЂРѕС€Рѕ      Р¶РёРІРµС‚     РЅР°     СЃРІРµС‚Рµ      Р’РёРЅРЅРё      РџСѓС…";
+	char str[] = "РђСЂРіРµРЅС‚РёРЅР° РјР°РЅРёС‚ РЅРµРіСЂР°";
 	cout << "Enter str: ";
 	SetConsoleCP(1251);
 	cin.getline(str, SIZE);
@@ -34,7 +34,7 @@ void main()
 	ToUpper(str);
 	cout << ToUpper(str) << endl;
 	cout << Shrink(str) << endl;
-	cout << "Строка " << str << (isPalindrom(str) ? " " : " Не") << " является палиндромом" << endl;
+	cout << "РЎС‚СЂРѕРєР° " << str << (isPalindrom(str) ? " " : " РќРµ") << " СЏРІР»СЏРµС‚СЃСЏ РїР°Р»РёРЅРґСЂРѕРјРѕРј" << endl;
 	if (Is_int_number(str)) cout << "to int number: " << To_int_number(str) + 222 << endl;
 	if (Is_bin_number(str)) cout << "bin_to_int: " << Bin_to_dec(str) << endl;
 	if (Is_hex_number(str)) cout << "Hex_to_int: " << hex_to_dec(str) << endl;
@@ -56,8 +56,8 @@ char* ToUpper(char str[])
 	for (int i = 0; str[i]; i++)
 	{/*
 		if (str[i] >= 'a' && str[i] <= 'z')str[i] -= ' ';
-		if (str[i] >= 'а' && str[i] <= 'я')str[i] -= ' ';
-		if (str[i] == 'ё')str[i] -= 16;*/
+		if (str[i] >= 'Р°' && str[i] <= 'СЏ')str[i] -= ' ';
+		if (str[i] == 'С‘')str[i] -= 16;*/
 		str[i] = toupper(str[i]);
 	}
 	return str;
@@ -204,7 +204,7 @@ int hex_to_dec(char str[])
 	return decimal;
 
 }
-//bool isIPaddress(const char str[])//проверка: only цифры и точки, only три точки, числа между точками от 0 до 255
+//bool isIPaddress(const char str[])//РїСЂРѕРІРµСЂРєР°: only С†РёС„СЂС‹ Рё С‚РѕС‡РєРё, only С‚СЂРё С‚РѕС‡РєРё, С‡РёСЃР»Р° РјРµР¶РґСѓ С‚РѕС‡РєР°РјРё РѕС‚ 0 РґРѕ 255
 //{
 //	int n = StringLength(str);
 //	int cont = 0;
@@ -339,7 +339,7 @@ bool isIPaddress(const char str[])
 //		}
 //		else return 0;
 //		bytes[0] = str[i]; // bytes[j++]=str[i];
-//		if (!Is_hex_number(bytes))return 0;//Второй вариант был - набрать сначала массив, а потом проверить, но в этом варианте не продолжаем - если не НЕХ...
+//		if (!Is_hex_number(bytes))return 0;//Р’С‚РѕСЂРѕР№ РІР°СЂРёР°РЅС‚ Р±С‹Р» - РЅР°Р±СЂР°С‚СЊ СЃРЅР°С‡Р°Р»Р° РјР°СЃСЃРёРІ, Р° РїРѕС‚РѕРј РїСЂРѕРІРµСЂРёС‚СЊ, РЅРѕ РІ СЌС‚РѕРј РІР°СЂРёР°РЅС‚Рµ РЅРµ РїСЂРѕРґРѕР»Р¶Р°РµРј - РµСЃР»Рё РЅРµ РќР•РҐ...
 //	}
 //	return 1/*(Is_hex_number(bytes) ? 1:0)*/;
 //}
@@ -360,5 +360,5 @@ bool isMACaddress(const char str[])
 		otrez=strtok(nullptr, ":. ");
 	}
 	delete[] buffer;
-	return 1;// по факту strtok нарезает str оставляя первый отрез, поэтому через буфер...
+	return 1;// РїРѕ С„Р°РєС‚Сѓ strtok РЅР°СЂРµР·Р°РµС‚ str РѕСЃС‚Р°РІР»СЏСЏ РїРµСЂРІС‹Р№ РѕС‚СЂРµР·, РїРѕСЌС‚РѕРјСѓ С‡РµСЂРµР· Р±СѓС„РµСЂ...
 }
